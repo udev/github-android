f270a76503b5175415879474033a9cf43d64a00e
Victor Ude <victor@primeequal.com>
2017-12-13 05:56:07 -0600

    cutting out some of the cruft

--- app/src/main/java/com/victorude/github/feature/search/SearchPresenter.kt
+++ app/src/main/java/com/victorude/github/feature/search/SearchPresenter.kt
@@ -13,15 +13,11 @@ import com.victorude.github.R
 import com.victorude.github.common.ARG_REPO
 import com.victorude.github.common.ARG_USER
 import com.victorude.github.feature.repo.RepoDetailFragment
-import com.victorude.github.model.Repo
-import com.victorude.github.model.Result
 import com.victorude.github.service.GitHubService
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -37,35 +33,27 @@ open class SearchPresenter @Inject constructor() : BasePresenterImpl<String>() {
 
         this.search = mvpView.findViewById(R.id.search)
 
-        val searchIntent: Disposable = getIntent().startWith("")
-                .skipWhile { queryString -> queryString.isNotBlank() }
-                .doOnNext({ queryString -> searchGithub(queryString) })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe()
+        val searchIntent: Disposable = getIntent()
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .flatMap { queryString ->
+                    Observable.fromCallable {
+                        github.search(mapOf("q" to queryString)).execute().body()
+                    }.observeOn(AndroidSchedulers.mainThread())
+                }
+                .subscribe({ results ->
+                    val resultsView: RecyclerView = mvpView.findViewById(R.id.results)
+                    resultsView.layoutManager = LinearLayoutManager(mvpView.context)
+                    resultsView.adapter = SearchResultAdapter(results!!, object : SearchItemClickListener() {
+                        override fun onItemClick(user: String, repo: String) {
+                            showDetails(user, repo)
+                        }
+                    })
+                }, { throwable ->
+                    Timber.e(throwable)
+                })
 
         compositeDisposable.add(searchIntent)
-    }
-
-    private fun searchGithub(queryString: String) {
-        github.search(mapOf("q" to queryString))
-                .enqueue(object : Callback<Result<List<Repo>>> {
-                    override fun onFailure(call: Call<Result<List<Repo>>>?, t: Throwable?) {
-                        //TODO: update error handling to notify the user
-                        Timber.tag("TAG").d(t?.message, t)
-                    }
-
-                    override fun onResponse(call: Call<Result<List<Repo>>>?, response: Response<Result<List<Repo>>>?) {
-                        val results: Result<List<Repo>> = response!!.body() ?: return
-                        val resultsView: RecyclerView = mvpView.findViewById(R.id.results)
-                        resultsView.layoutManager = LinearLayoutManager(mvpView.context)
-                        resultsView.adapter = SearchResultAdapter(results, object : SearchItemClickListener() {
-                                    override fun onItemClick(user: String, repo: String) {
-                                        showDetails(user, repo)
-                                    }
-
-                                })
-                    }
-                })
     }
 
     private fun showDetails(user: String, repo: String) {
@@ -81,7 +69,6 @@ open class SearchPresenter @Inject constructor() : BasePresenterImpl<String>() {
                 .commit()
     }
 
-    // handle search view events
     override fun getIntent(): Observable<String> {
         return RxSearchView.queryTextChangeEvents(search)
                 .filter { it.queryText().count() >= 3 }
